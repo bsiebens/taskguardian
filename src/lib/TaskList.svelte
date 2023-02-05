ccb0f63c82ba19b4ef7c4251cc243a3b8f250dc2
Bernard Siebens <bernard@siebens.org>
2023-02-05 17:53:33 +0100

    Added a default icon on sorting to show columns are sortable

--- src/lib/TaskList.svelte
+++ src/lib/TaskList.svelte
@@ -1,43 +1,43 @@
 <script>
-    import moment from 'moment';
-    import { createTable, Render, Subscribe, createRender } from 'svelte-headless-table';
-    import { addSortBy, addResizedColumns } from 'svelte-headless-table/plugins';
+    import moment from "moment";
+    import { createTable, Render, Subscribe, createRender } from "svelte-headless-table";
+    import { addSortBy, addResizedColumns } from "svelte-headless-table/plugins";
     import { tasks } from "./stores";
-    import TaskIcons from './TaskIcons.svelte';
-    import TaskDescription from './TaskDescription.svelte';
-    import TaskActions from './TaskActions.svelte';
+    import TaskIcons from "./TaskIcons.svelte";
+    import TaskDescription from "./TaskDescription.svelte";
+    import TaskActions from "./TaskActions.svelte";
     import { convertTaskwarriorDateToISO8601Format } from "./utilities";
-    import { IconArrowsSort, IconArrowUp, IconArrowDown, IconPencil, IconCheck, IconTrash } from '@tabler/icons-svelte';
+    import { IconArrowsSort, IconArrowUp, IconArrowDown, IconPencil, IconCheck, IconTrash } from "@tabler/icons-svelte";
 
     const priorityMapping = {
         undefined: 0,
         L: 1,
         M: 2,
-        H: 3
-    }
+        H: 3,
+    };
 
     function formatDateToRelativeDate(dateString) {
-        if (dateString === undefined) return '';
+        if (dateString === undefined) return "";
 
         return moment(convertTaskwarriorDateToISO8601Format(dateString)).fromNow();
     }
-    
+
     const table = createTable(tasks, {
-        sort: addSortBy({initialSortKeys: [{id: 'urgency', order: 'desc'}]}),
+        sort: addSortBy({ initialSortKeys: [{ id: "urgency", order: "desc" }] }),
         resize: addResizedColumns(),
     });
 
     const columns = table.createColumns([
         table.display({
-            id: 'icons',
-            header: '',
+            id: "icons",
+            header: "",
             cell: ({ row }) => createRender(TaskIcons, { item: row.original }),
         }),
         table.column({
-            header: 'Description',
+            header: "Description",
             accessor: (item) => item,
             cell: ({ value }) => createRender(TaskDescription, { item: value }),
-            id: 'description',
+            id: "description",
             plugins: {
                 sort: {
                     disable: true,
@@ -45,23 +45,23 @@
             },
         }),
         table.column({
-            header: 'Project',
-            accessor: 'project',
-            cell: ({ value }) => (value === undefined) ? '' : value,
+            header: "Project",
+            accessor: "project",
+            cell: ({ value }) => (value === undefined ? "" : value),
         }),
         table.column({
-            header: 'Priority',
-            accessor: 'priority',
-            cell: ({ value }) => (value === undefined) ? '' : value,
+            header: "Priority",
+            accessor: "priority",
+            cell: ({ value }) => (value === undefined ? "" : value),
             plugins: {
                 sort: {
                     getSortValue: (item) => priorityMapping[item],
-                }
+                },
             },
         }),
         table.column({
-            header: 'Due',
-            accessor: 'due',
+            header: "Due",
+            accessor: "due",
             cell: ({ value }) => formatDateToRelativeDate(value),
             plugins: {
                 sort: {
@@ -70,8 +70,8 @@
             },
         }),
         table.column({
-            header: 'Wait',
-            accessor: 'wait',
+            header: "Wait",
+            accessor: "wait",
             cell: ({ value }) => formatDateToRelativeDate(value),
             plugins: {
                 sort: {
@@ -80,21 +80,21 @@
             },
         }),
         table.column({
-            header: 'Urgency',
-            accessor: 'urgency',
+            header: "Urgency",
+            accessor: "urgency",
         }),
         table.display({
-            id: 'actions',
-            header: '',
+            id: "actions",
+            header: "",
             cell: ({ row }) => createRender(TaskActions, { item: row.original }),
-        })
+        }),
     ]);
-    
+
     const { headerRows, rows, tableAttrs, tableBodyAttrs } = table.createViewModel(columns);
 </script>
 
 <div class="table-container">
-    <table {...$tableAttrs} class='table table-hover'>
+    <table {...$tableAttrs} class="table table-hover">
         <thead>
             {#each $headerRows as headerRow (headerRow.id)}
                 <Subscribe rowAttrs={headerRow.attrs()} let:rowAttrs>
@@ -102,12 +102,14 @@
                         {#each headerRow.cells as cell (cell.id)}
                             <Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
                                 <th {...attrs} on:click={props.sort.toggle}>
-                                    <div class='flex flex-row'>
+                                    <div class="flex flex-row">
                                         <Render of={cell.render()} />
-                                        {#if props.sort.order === 'asc'}
-                                            <IconArrowUp class='ml-1' />
-                                        {:else if props.sort.order === 'desc'}
-                                            <IconArrowDown class='ml-1' />
+                                        {#if props.sort.order === "asc"}
+                                            <IconArrowUp class="ml-1" />
+                                        {:else if props.sort.order === "desc"}
+                                            <IconArrowDown class="ml-1" />
+                                        {:else if ["Project", "Priority", "Due", "Wait", "Urgency"].includes(cell.label)}
+                                            <IconArrowsSort class="text-surface-300 ml-1" />
                                         {/if}
                                     </div>
                                 </th>
