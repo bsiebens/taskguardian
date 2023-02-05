1233f3a07b7b90dd66961a9a2e0c10566499e480
Bernard Siebens <bernard@siebens.org>
2023-02-05 17:06:29 +0100

    Updated the TaskActions

--- src/lib/TaskActions.svelte
+++ src/lib/TaskActions.svelte
@@ -1,11 +1,18 @@
 <script>
-    import { IconPencil, IconCheck, IconTrash } from '@tabler/icons-svelte';
+    import { IconPencil, IconCheck, IconTrash, IconArrowBackUp, IconTrashOff } from "@tabler/icons-svelte";
 
     export let item;
 </script>
 
-<div class='flex flex-row gap-x-2'>
-    <IconPencil />
-    <IconCheck />
-    <IconTrash />
+<div class="flex flex-row gap-x-2">
+    {#if item.status == "completed"}
+        <IconArrowBackUp />
+        <IconTrash />
+    {:else if item.status == "deleted"}
+        <IconTrashOff />
+    {:else}
+        <IconPencil />
+        <IconCheck />
+        <IconTrash />
+    {/if}
 </div>
--- src/lib/TaskIcons.svelte
+++ src/lib/TaskIcons.svelte
@@ -1,22 +1,22 @@
 <script>
     import { convertTaskwarriorDateToISO8601Format } from "./utilities";
-    import { IconAlertCircle, IconAlertTriangle, IconLock, IconCheckbox, IconTrash } from '@tabler/icons-svelte';
+    import { IconAlertCircle, IconAlertTriangle, IconLock, IconCheckbox, IconTrash } from "@tabler/icons-svelte";
     import { pendingTasks } from "./stores";
 
     export let item;
-    
+
     function itemOverDue(item) {
-        if (item.status == 'completed') return false;
-        if (item.status == 'deleted' ) return false;
+        if (item.status == "completed") return false;
+        if (item.status == "deleted") return false;
 
         return new Date(convertTaskwarriorDateToISO8601Format(item.due)) < new Date();
     }
 
     function itemDueNext24Hours(item) {
-        if (item.status == 'completed' ) return false;
-        if (item.status == 'deleted' ) return false;
+        if (item.status == "completed") return false;
+        if (item.status == "deleted") return false;
 
-        return new Date(convertTaskwarriorDateToISO8601Format(item.due)) - new Date() <= (24 * 60 * 60 * 1000) // 24h * 60m * 60s * 1000ms
+        return new Date(convertTaskwarriorDateToISO8601Format(item.due)) - new Date() <= 24 * 60 * 60 * 1000; // 24h * 60m * 60s * 1000ms
     }
 
     function itemBlocked(item, pendingTasks) {
@@ -25,7 +25,7 @@
         var countBlockers = 0;
         for (let index = 0; index < item.depends.length; index++) {
             const dependency = item.depends[index];
-            
+
             if (pendingTasks.includes(dependency)) {
                 countBlockers++;
             }
@@ -36,22 +36,22 @@
     }
 </script>
 
-<div class='flex flex-row'>
-    {#if item.status == 'completed' || item.status == 'deleted'}
-        {#if item.status == 'completed'}
-            <IconCheckbox class='text-primary-500' />
+<div class="flex flex-row">
+    {#if item.status == "completed" || item.status == "deleted"}
+        {#if item.status == "completed"}
+            <IconCheckbox class="text-primary-500" />
         {:else}
-            <IconTrash class='text-secondary-500' />
+            <IconTrash class="text-secondary-500" />
         {/if}
     {:else}
         {#if itemOverDue(item)}
-            <IconAlertCircle class='text-error-500' />
+            <IconAlertCircle class="text-error-500" />
         {:else if itemDueNext24Hours(item)}
-            <IconAlertTriangle class='text-warning-500' />
+            <IconAlertTriangle class="text-warning-500" />
         {/if}
 
         <!-- {#if itemBlocked(item, $pendingTasks)}
-            <IconLock class='text-tertiary-500' />
+            <IconLock class="text-tertiary-500" />
         {/if} -->
     {/if}
 </div>
