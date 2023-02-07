eca926f0e1107b46fb26c63d7edb9e3cb99b11a4
Bernard Siebens <bernard@siebens.org>
2023-02-07 23:24:57 +0100

    Updated layour after prettier run

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,46 @@
 <script>
-    import { onMount } from 'svelte'
-    import { themeChange } from 'theme-change'
-    import Notifications from 'svelte-notifications'
-    import Alert from '../lib/Alert.svelte';
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+	import Notifications from 'svelte-notifications';
+	import Alert from '../lib/Alert.svelte';
 
-    import '../app.css'
+	import '../app.css';
 
-    onMount(() => {
-        themeChange(false)
-    });
+	onMount(() => {
+		themeChange(false);
+	});
 </script>
 
-<div class='navbar bg-neutral text-neutral-content'>
-    <div class='flex-1'>
-        <div class='ml-16 font-semibold normal-case text-2xl'>
-            <i class='fa-solid fa-shield-dog' />
-            TaskGuardian
-        </div>
-    </div>
-    <div class='flex-none gap-4 mr-32'>
-        <label class='swap swap-rotate'>
-            <input data-toggle-theme="halloween,emerald" data-act-class="active" type='checkbox' id='darkMode' />
-            <i class='fa-solid fa-sun swap-off fill-current w-6 h-6' />
-            <i class='fa-solid fa-moon swap-on fill-current w-6 h-6' />
-        </label>
-    </div>
+<div class="navbar bg-neutral text-neutral-content">
+	<div class="flex-1">
+		<div class="ml-16 text-2xl font-semibold normal-case">
+			<i class="fa-solid fa-shield-dog" />
+			TaskGuardian
+		</div>
+	</div>
+	<div class="mr-32 flex-none gap-4">
+		<label class="swap swap-rotate">
+			<input data-toggle-theme="halloween,emerald" data-act-class="active" type="checkbox" id="darkMode" />
+			<i class="fa-solid fa-sun swap-off h-6 w-6 fill-current" />
+			<i class="fa-solid fa-moon swap-on h-6 w-6 fill-current" />
+		</label>
+	</div>
 </div>
 
 <Notifications item={Alert}>
-    <div class='container mx-auto my-16'>
-        <div class='prose max-w-none'>
-            <slot />
-        </div>
-    </div>
+	<div class="container mx-auto my-16">
+		<div class="prose max-w-none">
+			<slot />
+		</div>
+	</div>
 </Notifications>
 
-<footer class='footer footer-center p-4 bg-base-300 text-base-content'>
-    <div>
-        <p>created by bernard siebens <a href='https://github.com/bsiebens/taskguardian' class='hover:text-secondary'><i class='fa-brands fa-github ml-2' /></a></p>
-    </div>
+<footer class="footer footer-center bg-base-300 p-4 text-base-content">
+	<div>
+		<p>
+			created by bernard siebens <a href="https://github.com/bsiebens/taskguardian" class="hover:text-secondary">
+				<i class="fa-brands fa-github ml-2" />
+			</a>
+		</p>
+	</div>
 </footer>
