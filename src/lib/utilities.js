41ed2bbe32adab6fea1075f5d0924799db89f71c
Bernard Siebens <bernard@siebens.org>
2023-02-07 23:22:48 +0100

    Added input types into utilities.js

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -1,4 +1,7 @@
 /* Check if a string is already ISO-8601 compliant. */
+/**
+ * @param {string} input_string
+ */
 function isISO8601(input_string) {
     const iso8601Regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?(Z|[+-]\d{2}:\d{2})$/
 
@@ -6,11 +9,14 @@ function isISO8601(input_string) {
 }
 
 /* Convert a date string from Taskwarrior to an ISO-8601 string. */
-export function convertTaskwarriorDateToISO8601Format(input_string, undefined_value=null) {
+/**
+ * @param {string | undefined} input_string
+ */
+export function convertTaskwarriorDateToISO8601Format(input_string, undefined_value = null) {
     if (input_string === undefined) {
         return (undefined_value === null) ? input_string : undefined_value
     }
-    
+
     if (isISO8601(input_string)) return input_string
 
     const year = input_string.slice(0, 4)
@@ -20,6 +26,6 @@ export function convertTaskwarriorDateToISO8601Format(input_string, undefined_va
     const minute = input_string.slice(11, 13)
     const second = input_string.slice(13, 15)
     const timezone = input_string.slice(15)
-    
+
     return `${year}-${month}-${day}T${hour}:${minute}:${second}${timezone}`
 }
