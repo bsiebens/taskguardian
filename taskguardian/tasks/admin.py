64d4e2669a6be936bd5732c4127a8279117bdec7
Bernard Siebens <bernard@siebens.org>
2023-06-11 16:08:52 +0200

    Creation of a new Django project

--- taskguardian/tasks/admin.py
+++ taskguardian/tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, Project
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "due", "priority", "created", "modified"]
+    list_display_links = ["name"]
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "created", "modified"]
+    list_display_links = ["name"]
--- taskguardian/tasks/models.py
+++ taskguardian/tasks/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from tree_queries.models import TreeNode
 
-# Create your models here.
+
+class Project(TreeNode):
+    name = models.CharField(max_length=250)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class PriorityChoices(models.TextChoices):
+        NONE = "none"
+        LOW = "low"
+        MEDIUM = "medium"
+        HIGH = "high"
+
+    name = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+
+    priority = models.CharField(max_length=6, choices=PriorityChoices.choices, default=PriorityChoices.NONE)
+    due = models.DateTimeField(blank=True, null=True)
+
+    project = models.ForeignKey(Project, blank=True, null=True, on_delete=models.SET_NULL)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
