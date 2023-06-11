ba4c16c4d3c15974a12894897605eca0cd972a2c
Bernard Siebens <bernard@siebens.org>
2023-06-11 20:24:55 +0200

    Building an urgency score model for tasks

--- taskguardian/tasks/admin.py
+++ taskguardian/tasks/admin.py
@@ -5,7 +5,7 @@ from .models import Task, Project
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "due", "priority", "created", "modified"]
+    list_display = ["id", "name", "due", "priority", "urgency", "created", "modified"]
     list_display_links = ["name"]
 
 
--- taskguardian/tasks/models.py
+++ taskguardian/tasks/models.py
@@ -1,5 +1,8 @@
+import datetime
+from decimal import Decimal
 from django.db import models
 from tree_queries.models import TreeNode
+from django.utils import timezone
 
 
 class Project(TreeNode):
@@ -32,3 +35,40 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+    def urgency(self):
+        value = 0.0
+
+        # Calculate urgency based on due date. If overdue for more than 7 days, value should be 1.0, if not due
+        # for another 2 weeks this value should 0.0. The final score is somewhere lineair in between these 2 values.
+        if self.due is not None:
+            SECONDS_PER_DAY = 24 * 60 * 60
+
+            due_date_difference = timezone.now() - self.due
+            due_date_difference_seconds = (
+                due_date_difference.days * SECONDS_PER_DAY + due_date_difference.seconds
+                if due_date_difference.days >= 0
+                else due_date_difference.days * SECONDS_PER_DAY - due_date_difference.seconds
+            )
+
+            if due_date_difference_seconds / SECONDS_PER_DAY >= 7:
+                value += 1.0
+            elif due_date_difference_seconds / SECONDS_PER_DAY >= -14:
+                value += (due_date_difference_seconds / SECONDS_PER_DAY * 0.8 / 21.0) + 0.2
+            else:
+                value += 0.2
+
+        # If tagged to project urgency increases by 1.0
+        if self.project is not None:
+            value += 1.0
+
+        # If priority is none = 0.0, low = 0.3, medium = 0.6, high = 1.0
+        if self.priority != self.PriorityChoices.NONE:
+            if self.priority == self.PriorityChoices.HIGH:
+                value += 1.0
+            elif self.priority == self.PriorityChoices.MEDIUM:
+                value += 0.6
+            else:
+                value += 0.3
+
+        return value
