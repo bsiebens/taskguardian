5702e69127b99fa7d798696c1645d66d9703e994
Bernard Siebens <bernard@siebens.org>
2023-06-11 09:01:41 +0200

    Restarting effort to build a Django backend

--- svelte_backup/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export NODE_ENV=production
-
-mkdir -p /run/nginx
-cd /src/taskguardian
-node build &
-nginx -g 'daemon off;'
--- tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import Task
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r"tasks", Task, basename="task")
-
-urlpatterns = router.urls
