1c5fcf145c87a0debf157c0683a9d825b552c3c0
Bernard Siebens <bernard@siebens.org>
2023-06-12 07:45:12 +0200

    Adding in django-ninja-extra for class-based API

--- taskguardian/taskguardian/api.py
+++ taskguardian/taskguardian/api.py
@@ -1,6 +1,10 @@
 from ninja import NinjaAPI
 from tasks.api import router as tasks_router
 
-api = NinjaAPI()
+api = NinjaAPI(
+    title="TaskGuardian API",
+    description="An API for managing and storing tasks. Inspired by TaskWarrior.",
+    version="0.0.1",
+)
 
 api.add_router("/tasks/", tasks_router)
--- taskguardian/tasks/api.py
+++ taskguardian/tasks/api.py
@@ -7,21 +7,22 @@ router = Router()
 
 
 class ProjectSchema(ModelSchema):
-    parent: "ProjectSchema" = None
-    full_name: str
+    project_path: List[str]
 
     class Config:
         model = Project
         model_fields = ["id", "name", "created", "modified"]
 
     @staticmethod
-    def resolve_full_name(obj):
-        return " - ".join([node.name for node in obj.ancestors(include_self=True)])
+    def resolve_project_path(obj):
+        return [node.name for node in obj.ancestors(include_self=True)]
 
 
 ProjectSchema.update_forward_refs()
 
 
-@router.get("/projects", response=List[ProjectSchema])
+@router.get("/projects", response=List[ProjectSchema], tags=["projects"])
 def list_projects(request):
+    """Returns a list of all projects. This list does not contain any information about the number of tasks
+    or the tasks themselves linked to these projects."""
     return Project.objects.with_tree_fields()
